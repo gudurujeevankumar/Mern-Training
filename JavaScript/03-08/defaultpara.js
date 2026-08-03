bf3751be43b3fe5a266630f96f4a7aba82be594b
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-03 18:46:29 +0530

    JS-FUN: Default parameterised functions

---
+++ JavaScript/03-08/defaultpara.js
@@ -0,0 +1,13 @@
+let sum = (a=3,b=4) => {
+    return a+b
+}
+let res = sum(10,20)
+console.log(res);
+
+let mul = (x=3) => console.log(x*x);
+mul();
+
+function d(a,b){
+    console.log(a,b,a+b)
+}
+d(null,null);
