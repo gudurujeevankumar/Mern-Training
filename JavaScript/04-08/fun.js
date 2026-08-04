40b5e761ee87e3b8e5070a63f2978a365bb3228d
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-04 16:18:07 +0530

    JS-FUN-STORE: storing returned value into variable ( let var = fun() ) and storing funcrtion in variable (let var = fun)

---
+++ JavaScript/04-08/fun.js
@@ -0,0 +1,15 @@
+
+// storing returned value into variable ( let var = fun() )
+function main(){
+    return "This is main function";
+}
+let r = main();
+console.log(r);
+
+
+// storing funcrtion in variable (let var = fun)
+function m1(){
+    return "This is m1 function"
+}
+let m = m1;
+console.log(m);
