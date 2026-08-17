fb4fe057f40ed223db25e672b6972d47358a0098
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-17 22:46:24 +0530

    JS-TASK: created nested arrays - 1. array[arrays] , 2.array[{objects}]

---
+++ JavaScript/17-08/arrayTask.js
@@ -0,0 +1,42 @@
+let students = [
+    ['SID', 'NAME', 'COURSE', 'BRANCH'],
+    ['A01', 'Jeevan', 'MERN', 'Jspiders'],
+    ['A02', 'Krishna', 'Python', 'Pyspiders'],
+    ['A03', 'Raju', 'Java', 'Qspiders'],
+    ['A04', 'Ravi', 'Data Analytics', 'Jspiders'],
+    ['A05', 'Ravali', 'MERN', 'Jspiders'],
+    ['A06', 'Seetha', 'Machine Learning', 'Pyspiders'],
+];
+
+let employees = [
+    {
+        EID: 'E101',
+        ENAME: 'Sathi',
+        DEPT: 'Development',
+        SAL: 1000
+    },
+    {
+        EID: 'E102',
+        ENAME: 'Ravi',
+        DEPT: 'Testing',
+        SAL: 1500
+    },
+    {
+        EID: 'E103',
+        ENAME: 'Priya',
+        DEPT: 'HR',
+        SAL: 2000
+    },
+    {
+        EID: 'E104',
+        ENAME: 'Kiran',
+        DEPT: 'Development',
+        SAL: 2500
+    },
+    {
+        EID: 'E105',
+        ENAME: 'Anu',
+        DEPT: 'Marketing',
+        SAL: 1800
+    }
+];
