27f09761f03494a34c708166796cac478bbbf3c1
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-19 11:21:37 +0530

    JS-ARRAY-METHODS: Array methods - push and pop, push will add element at last and return updated array length, pop remove the last element and return poped element

---
+++ JavaScript/19-08/arrayMethods.js
@@ -0,0 +1,10 @@
+let arr = [10,20,3,2231,323,1,-1];
+
+//push
+let pu = arr.push('pushed',[10,20]); // return updated array length
+console.log(arr,pu);
+
+//pop
+let po = arr.pop(); // returns poped element
+console.log(arr);
+console.log('Poped Element is ',po);
