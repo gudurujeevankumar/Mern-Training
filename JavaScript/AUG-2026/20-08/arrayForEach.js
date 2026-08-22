1ac3d14509216fc81f5b803994d22b886aeb56fd
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-22 11:10:27 +0530

    JS-FILE-MOD: organised into folders with month names

---
+++ JavaScript/AUG-2026/20-08/arrayForEach.js
@@ -0,0 +1,16 @@
+// let a = [10,20,30,40,50];
+
+// a.forEach((ele,ind,arrRef)=>{
+//     console.log(ele,ind,arrRef);
+// });
+
+// // sum of array
+
+// let sum = 0;
+
+// a.forEach(function(a,b){
+//     sum += a;
+// })
+// console.log(sum);
+
+
