d5bccb128dbe885df584054e1aa7a8fd6dbf3c6d
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-08-22 18:52:37 +0530

    JS-ARRAY-FILTER: Filter method iterate the array and returns the list of function length

---
+++ JavaScript/AUG-2026/22-08/arrFilter.js
@@ -0,0 +1,14 @@
+let arr = [12,9382,23,123,343]
+
+let fVal = arr.filter((ele,index,refArr)=>{
+    return ele < 300
+})
+
+console.log(fVal);
+
+let filVal = arr.filter((ind)=>{
+ 
+        return ind;
+    
+})
+console.log(filVal);
