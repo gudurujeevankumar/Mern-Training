d4b17eed97fa37d85dbbf72342858dce4f881d7a
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-09-01 11:25:33 +0530

    JS: Array Destructuring - storing the array values into variables at a time by easiely

---
+++ JavaScript/SEP-2026/arrayDestructuring.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4,5,6,7];
+
+let [a1,a2,a3,...a] = arr;
+console.log(a1,a2,a3,a);
+
+console.log(a);
+console.log("_________________");
+
+let brr = [10,30,24,45];
+console.log(brr)
+let [b1,,b3,...b] = brr;
+
+console.log(b1,b3,b);
