d6ee5d10b73c318eed51d61d9a1b814ddf61bc59
gudurujeevankumar <jeevankumarguduru3@gmail.com>
2026-09-02 18:27:02 +0530

    JS-OBJ: object destructuring,default values,asigning to new variable, by using rest parameter we can store multiple values, Nested object destructuring and  function parameter

--- JavaScript/SEP-2026/02-09/objectDestruc.js
+++ JavaScript/SEP-2026/02-09/objectDestruc.js
@@ -1,11 +1,53 @@
 // object destructuring 
 
 
-let s1 = {name: 'Priya' , age: 24 }
+let s1 = { name: 'Priya', age: 24 }
 
-let { name , age} = s1;
+let { name, age } = s1;
 
 console.log(name);
 console.log(age);
 
 
+// default values
+
+let { marks } = s1;
+console.log(marks) // Undefined
+
+let { remark = 'Good' } = s1;
+console.log(remark); // we'll get Good as output even we don't have remark property in object
+
+// asigning to new variable
+
+let s2 = { sname: 'Jeevan', age: 23, course: 'MERN', city: 'Bangalore' };
+
+let { sname: peru } = s2;
+let { age: vayasu } = s2;
+
+console.log(peru);
+console.log(vayasu);
+
+// by using rest parameter we can store multiple values
+
+let { ...stu } = s2;
+console.log(stu);
+
+// Nested object destructuring
+let emp = { 
+    ename: "Kooli", 
+    doj: '12-04-2014', 
+    address : {
+        city: 'ATP',
+        state: 'AP'
+    }
+}
+
+let {address:{city,state}} = emp
+console.log(city);
+console.log(state);
+
+// function 
+function main({ename,doj}){
+    console.log(ename,doj);
+}
+main(emp)
